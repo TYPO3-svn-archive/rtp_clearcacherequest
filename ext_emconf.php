addc10ba911c793d5e781e84b4bb85b69c4809fe
rtpartner <rtpartner@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-07-21 20:19:33 +0000

    [INFO] Bumped version no.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/rtp_clearcacherequest/trunk@35916 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "rtp_clearcacherequest".
 #
-# Auto generated 21-07-2010 15:16
+# Auto generated 21-07-2010 22:13
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.2.0',
+	'version' => '1.3.0',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"1caa";s:10:"README.txt";s:4:"ee2d";s:21:"ext_conf_template.txt";s:4:"8745";s:12:"ext_icon.gif";s:4:"e049";s:17:"ext_localconf.php";s:4:"0127";s:24:"ext_typoscript_setup.txt";s:4:"6d3d";s:41:"lib/class.tx_rtpclearcacherequest_div.php";s:4:"a517";s:41:"pi1/class.tx_rtpclearcacherequest_pi1.php";s:4:"0131";s:22:"res/example_script.php";s:4:"3326";}',
+	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"1caa";s:10:"README.txt";s:4:"ee2d";s:21:"ext_conf_template.txt";s:4:"5ed2";s:12:"ext_icon.gif";s:4:"8f5f";s:17:"ext_localconf.php";s:4:"0127";s:24:"ext_typoscript_setup.txt";s:4:"6d3d";s:14:"doc/manual.sxw";s:4:"ed8d";s:41:"lib/class.tx_rtpclearcacherequest_div.php";s:4:"0b3d";s:41:"pi1/class.tx_rtpclearcacherequest_pi1.php";s:4:"9e1c";s:22:"res/example_script.php";s:4:"1b78";}',
 	'suggests' => array(
 	),
 );
